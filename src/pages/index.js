2c735fb6080a02b918926234544ef2a4f72681eb
Hai Liang Wang <hain@chatopera.com>
2022-09-14 05:31:00 +0100

    https://github.com/cskefu/cskefu-docs/issues/11 fix deply link

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/">
+            to="/docs/deploy">
             应用部署 - 5 分钟 ⏱️
           </Link>
         </div>
